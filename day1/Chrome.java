ca59570d0d3c27f62932e5a99e54650aa02b255d
karthikbmr <karthikk7576@gmail.com>
2024-12-07 22:43:40 +0530

    Add files via upload
    week1.day1.assignments

---
+++ day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		
+		System.out.println("Browser Version is: "+version);
+		System.out.println("Page is: "+developer);
+		System.out.println("Browser Stable: "+isBeta);
+		System.out.println("Browser Released in: "+releaseYear);
+		System.out.println("Short Key is: "+shortcutKey);
+		
+	}
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox {
+	
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName = "firefox"; 
+		boolean	isVisible = true ;
+		int	releaseYear = 1998 ;
+		char browserLogo = 'f';
+		
+		System.out.println("Browser is:"+browserName);
+		System.out.println("Browser Version is: "+browserVersion);
+		System.out.println("Browser avaiable: "+isVisible);
+		System.out.println("Browser Released in: "+releaseYear);
+		System.out.println("Browser Logo is: "+browserLogo);
+		
+	}
+
+}
